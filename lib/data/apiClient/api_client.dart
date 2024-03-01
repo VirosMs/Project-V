9fbab7f79ea886fb623075036a57bd144411e83c
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2024-03-01 17:21:11 +0100

    Añadida doc para los archivos de la caperta Data

--- lib/data/apiClient/api_client.dart
+++ lib/data/apiClient/api_client.dart
@@ -1,3 +1,4 @@
 import 'package:project_v/core/app_export.dart';
 
+/// This class represents an API client for making network requests.
 class ApiClient {}
--- lib/data/models/selectionPopupModel/selection_popup_model.dart
+++ lib/data/models/selectionPopupModel/selection_popup_model.dart
@@ -1,11 +1,16 @@
-///SelectionPopupModel is common model
-///used for setting data into dropdowns
+/// SelectionPopupModel is a common model used for setting data into dropdowns.
 class SelectionPopupModel {
   int? id;
   String title;
   dynamic value;
   bool isSelected;
 
+  /// Constructs a new instance of [SelectionPopupModel].
+  ///
+  /// The [id] is an optional parameter representing the unique identifier of the model.
+  /// The [title] is a required parameter representing the title of the model.
+  /// The [value] is an optional parameter representing the value associated with the model.
+  /// The [isSelected] is an optional parameter indicating whether the model is selected or not.
   SelectionPopupModel({
     this.id,
     required this.title,
