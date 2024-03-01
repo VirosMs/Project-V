3453179690871feb816fe0e744d09f0f237d9056
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2024-03-01 17:20:20 +0100

    Agrega documentacion Localization

--- lib/localization/app_localization.dart
+++ lib/localization/app_localization.dart
@@ -3,23 +3,30 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import '../core/app_export.dart';
 
+/// A class that handles localization for the app.
 class AppLocalization {
   AppLocalization(this.locale);
 
+  /// The current locale.
   Locale locale;
 
   static final Map<String, Map<String, String>> _localizedValues = {'en': enUs};
 
+  /// Retrieves the instance of [AppLocalization].
   static AppLocalization of() {
     return Localizations.of<AppLocalization>(
         NavigatorService.navigatorKey.currentContext!, AppLocalization)!;
   }
 
+  /// Retrieves a list of supported languages.
   static List<String> languages() => _localizedValues.keys.toList();
+
+  /// Retrieves the localized string for the given [text].
   String getString(String text) =>
       _localizedValues[locale.languageCode]![text] ?? text;
 }
 
+/// A delegate for the [AppLocalization] class.
 class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalization> {
   const AppLocalizationDelegate();
 
@@ -27,8 +34,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalization> {
   bool isSupported(Locale locale) =>
       AppLocalization.languages().contains(locale.languageCode);
 
-  //Returning a SynchronousFuture here because an async "load" operation
-  //cause an async "load" operation
+  // Returning a SynchronousFuture here because an async "load" operation
+  // would cause an error.
   @override
   Future<AppLocalization> load(Locale locale) {
     return SynchronousFuture<AppLocalization>(AppLocalization(locale));
@@ -38,6 +45,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<AppLocalization> {
   bool shouldReload(AppLocalizationDelegate old) => false;
 }
 
+/// An extension on [String] that provides a shorthand for retrieving localized strings.
 extension LocalizationExtension on String {
+  /// Retrieves the localized string for the current [String].
   String get tr => AppLocalization.of().getString(this);
 }
